529e88480e50537a0ab75b05d8a49d2a2b884f7c
evelyn lmeida <evelynalmeida260@gmail.com>
2024-09-16 15:07:57 -0300

    Criamos a interface inicial do App Contatos

---
+++ contatos/src/main/java/com/etecriopardo/contatos/Contatos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.etecriopardo.contatos;
+
+/**
+ *
+ * @author Etec
+ */
+public class Contatos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
